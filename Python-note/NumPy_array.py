352f568676968e41ee2a79656106e2b1914e3828
qifeibro <87322305+qifeibro@users.noreply.github.com>
2023-02-14 15:08:05 +0000

    Add files via Codespaces

--- Python-note/NumPy_array.py
+++ Python-note/NumPy_array.py
@@ -1,3 +1,8 @@
+"""
+broadcasting refers to a numpy array's ability to vectorize operations,
+so they are performed on all elements of an object at once.
+"""
+
 # Alternative to Python lists
 # num_list = list(range(5))
 
@@ -70,3 +75,22 @@ print(pos) # [1, 2]
 # List comprehension (better option but not best)
 pos = [num for num in nums if num > 0]
 print(pos) # [1, 2]
+
+
+# Practice with NumPy arrays
+nums = np.array( [ [1, 2, 3, 4, 5],
+                   [6, 7, 8, 9, 10 ] ] )
+# Print second row of nums
+print(nums[1,:])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+# with a new column that adds 1 to each item in the original column
+nums[:,2] = nums[:,2] + 1
+print(nums)
